a9d291550ef34ce8dde7734f45ef195d895c8383
Panchesco <panchesco@panchesco.com>
2018-06-14 16:27:20 -0700

    More documentation.

--- panchco-simple-event.php
+++ panchco-simple-event.php
@@ -6,14 +6,33 @@
  * @package panchco_simple_event
  * @author Richard Whitmer
  * @since 1.0.0
-*/
-
-/**
-Plugin Name: Panchco Simple Event
-Plugin URI: https://github.com/Panchesco/panchco-simple-event
-Description: Add date, time, duration, and archive information to any post type.
-Author: Richard Whitmer
-Version: 1.2.2
+ *
+ * Plugin Name: Panchco Simple Event
+ * Plugin URI: https://github.com/Panchesco/panchco-simple-event
+ * Description: Add date, time, duration, and archive information to any post type.
+ * Author: Richard Whitmer
+ * Version: 1.2.2
+ * License: MIT
+ * Copyright 2018, Richard Whitmer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files 
+ * (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, 
+ * and to permit persons to whom the Software is furnished to do so, 
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 // Exit if accessed directly.
